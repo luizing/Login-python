20d4738b6262d6daeae733ee137114706b3df602
luiz <luizeduardo2099@gmail.com>
2021-09-17 22:37:11 -0300

    apagar usuario update

--- logado.py
+++ logado.py
@@ -2,6 +2,7 @@ def init():
     print('Bem Vindo!')
     print('Selecione uma opção:')
     print("1 - Alterar Senha")
+    print("2 - Apagar Usuário")
     op = int(input("-> "))
 
     if op == 1:
@@ -21,6 +22,24 @@ def init():
                             linha = elementos[0] + ',' + elementos[1] + "," + "\n"
                     linha = elementos[0] + ',' + elementos[1] + "," + "\n"
                     arquivo.write(linha)
+
+
+    if op == 2:
+        print("Digite seu usuário e senha novamente.")
+        usuario = input("Usuário: ")
+        senha = input("Senha: ")
+
+        with open('usuarios.txt', 'r') as arquivo:
+            linhas = arquivo.readlines()
+            with open('usuarios.txt', 'w') as arquivo:
+                for linha in linhas:
+                    elementos = linha.split(',')
+                    if elementos[0] == usuario and elementos[1] == senha:
+                            linha =''
+                            arquivo.write(linha)
+                    else:
+                        linha = elementos[0] + ',' + elementos[1] + "," + "\n"
+                        arquivo.write(linha)
 
 
 
